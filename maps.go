6d616fcacf788b1a27139db307c1e906507585e7
user <haghee0eip6ohjoh@protonmail.com>
2022-08-15 21:37:49 +0500

    method(maps): new MapValues method

--- maps.go
+++ maps.go
@@ -18,12 +18,34 @@ func ConvertMap[K1 comparable, V1 any, K2 comparable, V2 any](
 	return converted, nil
 }
 
-func MapKeys[T1 comparable, T2 any](m map[T1]T2) []T1 {
-	l := make([]T1, 0, len(m))
+// MapKeys returns a slice []KeyType with the keys of the map[KeyType]ValueType object.
+func MapKeys[KeyType comparable, ValueType any](m map[KeyType]ValueType) []KeyType {
+	l := make([]KeyType, len(m))
+	i := 0
 
 	for k := range m {
-		l = append(l, k)
+		l[i] = k
+		i++
 	}
 
 	return l
+}
+
+// MapValues returns a slice []ValueType with the values of the map[KeyType]ValueType object.
+func MapValues[KeyType comparable, ValueType any](m map[KeyType]ValueType) []ValueType {
+	l := make([]ValueType, len(m))
+	i := 0
+
+	for _, v := range m {
+		l[i] = v
+		i++
+	}
+
+	return l
+}
+
+func MapHas[KeyType comparable, ValueType any](m map[KeyType]ValueType, k KeyType) bool {
+	_, has := m[k]
+
+	return has
 }
