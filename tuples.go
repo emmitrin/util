a640c124112978d601d8cdab3b2d888ab874fff0
user <haghee0eip6ohjoh@protonmail.com>
2022-10-08 18:02:52 +0500

    tuples utility

--- tuples.go
+++ tuples.go
@@ -7,3 +7,20 @@ func First[T1, T2 any](a T1, b T2) T1 {
 func Second[T1, T2 any](a T1, b T2) T2 {
 	return b
 }
+
+type Tuple[T1, T2 any] struct {
+	first  T1
+	second T2
+}
+
+func (t *Tuple[T1, T2]) First() T1 {
+	return t.first
+}
+
+func (t *Tuple[T1, T2]) Second() T2 {
+	return t.second
+}
+
+func NewTuple[T1, T2 any](first T1, second T2) *Tuple[T1, T2] {
+	return &Tuple[T1, T2]{first, second}
+}
